b9e2f61a7d37ef3d1f4f782db974ff23493cd542
met-tahe <met-tahe@student.1337.ma>
2023-12-30 19:54:28 +0100

    CPP_POOL

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -19,9 +19,31 @@ int main()
     --it;
     while (it != ite)
     {
-    std::cout << *it << std::endl;
-    ++it;
+        std::cout << *it << std::endl;
+        ++it;
     }
     std::stack<int> s(mstack);
+    std::cout<<"----------------------TEST WITH VECTORS-----------------------"<<std::endl;
+    std::vector<int>vec;
+    vec.push_back(5);
+    vec.push_back(17);
+    std::cout << vec[vec.size()-1]<< std::endl;
+    vec.pop_back();
+    std::cout << vec.size() << std::endl;
+    vec.push_back(3);
+    vec.push_back(5);
+    vec.push_back(737);
+    //[...]
+    vec.push_back(0);
+    std::vector<int>::iterator it1 = vec.begin();
+    std::vector<int>::iterator ite1 = vec.end();
+    ++it1;
+    --it1;
+    while (it1 != ite1)
+    {
+        std::cout << *it1 << std::endl;
+        ++it1;
+    }
+   // std::vector<int> s(vec);
     return 0;
 }
